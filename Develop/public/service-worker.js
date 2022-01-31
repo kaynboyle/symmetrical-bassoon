5f5ead8a8672f00cb8268bf6a371cb733f58672b
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-01-31 05:04:52 -0800

    manifest json added as well as idb.js, items are now saved to cache

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,23 +1,78 @@
-const APP_PREFIX = 'BudgetPlanner-';     
-const VERSION = 'version_01';
+const VERSION = 'version1';
+const APP_PREFIX = 'cache';  
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE = [
-    // "./Develop/public/index.html",
-    // "./Develop/public/css/styles.css",
- // do i need to load icons in here?
-    "./public/icons/icon-152x152.png"
-   
-  ];
+const FILES_LIST = [
+    "/",
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+];
 
-self.addEventListener('install', function (e){
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache){
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-})
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
-///why wont this work 
-// hints at 19.4.7
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_LIST);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+      })
+    );
+    return;
+  }
+
+  // default
+//   event.respondWith(
+//     fetch(event.request).catch(function() {
+//       return caches.match(event.request).then(function(response) {
+//         if (response) {
+//           return response;
+//         } else if (event.request.headers.get("accept").includes("text/html")) {
+//           return caches.match("/");
+//         }
+//       });
+//     })
+//   );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cache = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      cache.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cache.indexOf(key) === -1) {
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
+});
